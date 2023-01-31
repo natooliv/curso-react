84a648ae9e5adb1b72f2140e7b28fc10e3a7ddb3
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2023-01-31 18:13:52 -0300

    Desafio Repetição

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -8,11 +8,21 @@ import Fragmento from './components/basicos/Fragmento';
 import Card from './components/layout/Card';
 import Familia from './components/basicos/Familia';
 import FamiliaMembro from './components/basicos/FamiliaMembro';
+import ListaAlunos from './components/basicos/Repetição/ListaAlunos';
+import TabelaProdutos from './components/basicos/Repetição/TabelaProdutos';
 // eslint-disable-next-line no-unused-expressions
 export default () => (
   <div className='App'>
     <h1>Fundamentos React</h1>
     <div className='Cards'>
+
+    <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
+                <TabelaProdutos></TabelaProdutos>
+            </Card>
+
+      <Card titulo='#06-Repetição' color='#FF4C65'>
+      <ListaAlunos></ListaAlunos>
+      </Card>
 
       <Card titulo='#05-Componentes com Filhos' color='#00C8F8'>
         <Familia sobrenome='Ferreira'>
@@ -20,7 +30,7 @@ export default () => (
           <FamiliaMembro nome='Pedro' />
           <FamiliaMembro nome='Ana' />
           <FamiliaMembro nome='Gustavo' />
-          
+
         </Familia>
       </Card>
       <Card titulo='#04-Desafio Aleatório' color='#FA6900'>
