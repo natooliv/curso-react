37b713d15f1d8209ab7e4e4e3652af5036ffa929
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2023-01-31 16:06:30 -0300

    Component Card

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css';
 import Primeiro from './components/basicos/Primeiro';
 import Aleatorio from './components/basicos/Aleatorio';
 // import Comp from './components/basicos/Primeiro'; iria funcionar normalmente
@@ -7,22 +8,27 @@ import Fragmento from './components/basicos/Fragmento';
 import Card from './components/layout/Card';
 // eslint-disable-next-line no-unused-expressions
 export default () => (
-  <div id='app'>
+  <div className='App'>
     <h1>Fundamentos React</h1>
-    <Card titulo='#05-Desafio Aleatório'>
-      <Aleatorio min={10} max={60} />
-    </Card>
-    <Card titulo='#04-Fragmento'>
-      <Fragmento />
-    </Card>
-    <Card titulo='#03-Com parametro'>
-      <ComParametro
-        titulo="#02-Situação do aluno"
-        aluno="Pedro " nota='9.3' />
-    </Card>
-    <Card titulo='#01-Primeiro'>
-      <Primeiro></Primeiro>
-    </Card>
+    <div className='Cards'>
 
+      <Card titulo='#05-Desafio Aleatório' color='#FA6900'>
+        <Aleatorio min={10} max={60} />
+      </Card>
+
+      <Card titulo='#04-Fragmento'color='#E94C6F'>
+        <Fragmento />
+      </Card>
+
+      <Card titulo='#03-Com parametro'color='#E8B71A'>
+        <ComParametro
+          titulo="#02-Situação do aluno"
+          aluno="Pedro " nota='9.3' />
+      </Card>
+
+      <Card titulo='#01-Primeiro'color='#588C73'>
+        <Primeiro></Primeiro>
+      </Card>
+    </div>
   </div>
 );
--- fundamentos-react/src/components/layout/Card.jsx
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import "./Card.css"
 export default (props) => {
-  return(
-  <div className='Card'>
+     return(
+  <div className='Card' style={{
+    backgroundColor: props.color ||'#008',
+   borderColor: props.color || '#008',
+  }}>
     <div className='Title'></div>
     <div className='Content'>{props.titulo}</div>
     {props.children}
